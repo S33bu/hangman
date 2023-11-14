6c83f9bc8981743a79e828faa6580003df8a341f
S33bu <sebastianmais@outlook.com>
2023-11-14 22:30:21 +0200

    kirjutasin mingit jama kokku, ei saanud aru kuidas ilma listita mängu printida, aga teoorias mäng toimib

--- mäng.py
+++ mäng.py
@@ -1,1 +1,22 @@
-asdadsdsa
+import random
+
+pildid = []
+
+sõnad = ["õun", "jõgi", "laud", "kott", "nupp", "uks", "käsi",
+          "kell", "tuli", "rohi", "nina", "lill", "tore", "pilt", "päev"]
+
+mängusõna = random.choice(sõnad) #valib mingi sõna üleval olevast listist
+mängusõna1 = list(mängusõna)
+elud = len(mängusõna) #elusid on sama palju kui tähti sõnas
+peidetud_sõna = len(mängusõna) * "_" #siin nt kui sõna on kott ja sa pakud k siis -> k _ _ _
+peidetud_sõna1 = list(peidetud_sõna)
+
+print(mängusõna, elud)
+
+while elud > 0:
+    print ("Arva sõna ära!: ", peidetud_sõna1)
+    täht = input("Paku tähte!: ")
+    if täht in mängusõna:
+        for i in range (len(mängusõna)):
+            if mängusõna1[i] == täht:
+                peidetud_sõna1[i] = täht
